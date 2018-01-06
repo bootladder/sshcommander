e28ba3cffeb7c65ef0488d08700b7c84d6ac33fc
bootladder <steve@odi.net>
2018-01-05 18:58:23 -0800

    CreateCommandString

--- sshcommander.go
+++ sshcommander.go
@@ -1,6 +1,7 @@
 package sshcommander
 import (
-  //"errors"
+  "fmt"
+  "strings"
 )
 type SSHCommander struct {
   User, Hostname string
@@ -8,7 +9,17 @@ type SSHCommander struct {
   Key string
 }
 
-func (s *SSHCommander) Command(commandstring string) (err error) {
 
-  return 
+func (s *SSHCommander) Command(commandstring string) (err error) {
+  return
+}
+
+func (s *SSHCommander) CreateCommandString(commandstring string) (out string, err error) {
+
+  cmd := []string{
+    fmt.Sprintf("ssh -p %d %s@%s", s.Port, s.User, s.Hostname),
+  }
+  cmd = append(cmd, " ", "\"",commandstring, "\"")
+
+  return  strings.Join(cmd,""),nil
 }
--- sshcommander_test.go
+++ sshcommander_test.go
@@ -1,6 +1,5 @@
 package sshcommander_test
 import (
-  "fmt"
   "testing"
   "sshcommander"
   "github.com/stretchr/testify/assert"
@@ -10,5 +9,21 @@ func TestNewSSHCommander( t *testing.T) {
   commander := sshcommander.SSHCommander{"root", "localhost", 22, ""}
   err := commander.Command("echo hellocommand")
   assert.Equal(t, err, nil)
-    fmt.Print("hello")
 }
+//////////////////////////////////////////////////////////////////////
+func TestCreateCommandString( t *testing.T) {
+  commander := sshcommander.SSHCommander{"root", "localhost", 22, ""}
+  actualResult, _ := commander.CreateCommandString("echo hellocommand")
+  assert.Equal(t, "ssh -p 22 root@localhost \"echo hellocommand\"", actualResult)
+}
+func TestCreateCommandStringSetsTargetCommand( t *testing.T) {
+  commander := sshcommander.SSHCommander{"root", "localhost", 22, ""}
+  actualResult, _ := commander.CreateCommandString("echo a different command")
+  assert.Equal(t, "ssh -p 22 root@localhost \"echo a different command\"", actualResult)
+}
+func TestCreateCommandStringSetsPortHostUser( t *testing.T) {
+  commander := sshcommander.SSHCommander{"myuser", "differenthost", 20010, ""}
+  actualResult, _ := commander.CreateCommandString("echo hellocommand")
+  assert.Equal(t, "ssh -p 20010 myuser@differenthost \"echo hellocommand\"", actualResult)
+}
+//////////////////////////////////////////////////////////////////////
