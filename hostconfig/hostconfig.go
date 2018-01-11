d81828d12d8842e8a046e73cb6e6bfa3cc25a969
bootladder <steve@odi.net>
2018-01-11 14:30:53 -0800

    testing reading json into struct

--- hostconfig/hostconfig.go
+++ hostconfig/hostconfig.go
@@ -1,23 +1,40 @@
 package hostconfig
 
 import (
-  //"errors"
+  "errors"
+  "encoding/json"
   "io/ioutil"
   "fmt"
 )
 
+type HostConfigFile struct {
+	Name string `json:"name"`
+	Host struct {
+		Hostname string `json:"hostname"`
+		Port     int    `json:"port"`
+		Key      string `json:"key"`
+		User     string `json:"user"`
+	} `json:"host"`
+}
+
+var myhostconfig HostConfigFile
 
 func Load(pathtoconfigfile string) (err error) {
+    fmt.Println("Loading Conf File into Memory")
     raw, err := ioutil.ReadFile(pathtoconfigfile)
     if err != nil {
-        fmt.Println(err.Error())
         return
     }
-    fmt.Println("%s",raw)
-
-  //return errors.New("i'm failing here")
-  return nil
+    fmt.Printf("%s",raw)
+    json.Unmarshal(raw, &myhostconfig)
+    fmt.Printf("Results: %v\n", myhostconfig)
+    fmt.Printf("name: %v\n", myhostconfig.Name)
+    //return errors.New("i'm failing here")
+    return nil
 }
 func LookupHostname(hostname string) (err error) {
-  return nil
+  if hostname == myhostconfig.Name {
+      return
+  }
+  return errors.New("i'm failing here")
 }
---
+++ hostconfig/hostconfig_test.go
@@ -0,0 +1,42 @@
+package hostconfig_test
+
+import(
+  "github.com/bootladder/sshcommander/hostconfig"
+  "github.com/stretchr/testify/assert"
+  "os/user"
+  "fmt"
+  "testing"
+)
+
+func TestLoadDefaultConfig( t *testing.T) {
+
+  usr, _ := user.Current()
+  dir := usr.HomeDir
+  err := hostconfig.Load(dir + "/.sshcommander/hostconfig.json")
+  assert.Equal(t, nil, err)
+}
+
+func TestLoadConfigInvalidFilename( t *testing.T) {
+
+  err := hostconfig.Load("/my/crappy/filename")
+  assert.NotEqual(t, nil, err)
+}
+
+func TestLookupHostnameInvalidReturnsError( t *testing.T) {
+
+  usr, _ := user.Current()
+  dir := usr.HomeDir
+  err := hostconfig.Load(dir + "/.sshcommander/hostconfig.json")
+  err = hostconfig.LookupHostname("invalidhostname")
+  assert.NotEqual(t, nil, err)
+}
+
+func TestLookupHostnameValid( t *testing.T) {
+
+  err := hostconfig.Load("./samplehostconfig.json")
+  if err != nil {
+    fmt.Println(err)
+  }
+  err = hostconfig.LookupHostname("myfirsthostname")
+  assert.Equal(t, nil, err)
+}
