cb01c5b5e4e5c0fb7dcaf590e2985b1488b1f7af
bootladder <steve@odi.net>
2018-01-11 02:17:52 -0800

    example commandline creator

--- sshcommander_example.go
+++ sshcommander_example.go
@@ -1,25 +1,13 @@
 package main
 
 import(
-  "sshcommander"
+  "sshcommander/sshcommandcreator"
   "fmt"
-  "os/exec"
 )
-type RealOSCommandExecuter struct {}
-
-func (r RealOSCommandExecuter) Execute(commandstring string) (out string, err error) {
-  bytesout,err := exec.Command("sh" , "-c" , commandstring).Output()
-  out = string(bytesout)
-  return
-}
 
 func main() {
-  realCommandExecuter := RealOSCommandExecuter{}
-  sshcommander.InjectOSCommandExecuter(realCommandExecuter)
-  commander := sshcommander.SSHCommander{"root", "209.148.82.100", 22, ""}
-
-  out, _ := commander.Command("cat /etc/issue")
-
+  creator := sshcommandcreator.SSHCommandCreator{"root", "111.111.11.111", 22, ""}
+  out, _ := creator.CreateCommandString("cat /etc/issue")
   fmt.Println(out)
 }
 
