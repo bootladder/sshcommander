bc5efd7328444f3d2ad69aef5bb73d70f27134e9
bootladder <steve@odi.net>
2018-01-12 01:37:42 -0800

    nesting test worked

--- sshcommandcreator/sshcommandcreator_test.go
+++ sshcommandcreator/sshcommandcreator_test.go
@@ -27,3 +27,11 @@ func TestCreateCommandStringSetsPortHostUser( t *testing.T) {
   assert.Equal(t, "ssh -p 20010 myuser@differenthost echo hellocommand", actualResult)
 }
 //////////////////////////////////////////////////////////////////////
+//Nesting Tests
+func TestCreateCommandStringNestedWorks( t *testing.T) {
+  creator1 := sshcommandcreator.SSHCommandCreator{"myuser", "differenthost", "20010", ""}
+  creator2 := sshcommandcreator.SSHCommandCreator{"myother", "otherhost", "10099", ""}
+  actualResult1, _ := creator1.CreateCommandString("echo hellocommand")
+  actualResult2, _ := creator2.CreateCommandString(actualResult1)
+  assert.Equal(t, "ssh -p 10099 myother@otherhost ssh -p 20010 myuser@differenthost echo hellocommand", actualResult2)
+}
